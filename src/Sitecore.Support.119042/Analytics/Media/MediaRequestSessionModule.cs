4625768feceea37c5e2609860212d5bbcb8e48e3
DK\YMU <ymu@sitecore.net>
2017-12-12 15:06:40 +0200

    Original source code and VS files

---
+++ src/Sitecore.Support.119042/Analytics/Media/MediaRequestSessionModule.cs
@@ -0,0 +1,34 @@
+﻿using Sitecore.Analytics.Media;
+using Sitecore.Resources.Media;
+using System;
+using System.Web;
+using Sitecore.Analytics.Web;
+
+namespace Sitecore.Support.Analytics.Media
+{
+  public class MediaRequestSessionModule : Sitecore.Analytics.Media.MediaRequestSessionModule
+  {
+    protected override bool IsSessionRequired()
+    {
+      if (!this.IsTrackerEnabled())
+      {
+        return false;
+      }
+      MediaRequest request = MediaManager.ParseMediaRequest(HttpContext.Current.Request);
+      if (request == null)
+      {
+        return false;
+      }
+      MediaRequestTrackingInformation information = new MediaRequestTrackingInformation(request);
+      if (information.IsTrackedRequest())
+      {
+        return true;
+      }
+      ContactKeyCookie cookie = new ContactKeyCookie("");
+      return !cookie.IsClassificationGuessed;
+    }
+
+
+
+  }
+}
---
+++ src/Sitecore.Support.119042/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.119042")]
+[assembly: AssemblyProduct("Sitecore.Support.119042")]
+[assembly: ComVisible(false)]
